8c9d8a01c112bd76b8a121af8b3cd240cb4af854
HuihuiPoter <x_huihui@qq.com>
2020-12-25 16:03:02 +0800

    re

---
+++ src/components/Result.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <el-table :data="stat" border stripe>
+            <el-table-column v-for="col in columns"
+                :prop="col.id"
+                :key="col.id"
+                :label="col.label"
+                :width="col.width">
+            </el-table-column>
+        </el-table>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Result',
+    data() {
+        return {
+            stat: []
+        }
+    }
+}
+</script>
